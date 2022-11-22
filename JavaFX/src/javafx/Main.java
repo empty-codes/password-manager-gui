a7c42afd0e114b50d5ae7e127f025084d7ba66ea
empty-codes <92234322+empty-codes@users.noreply.github.com>
2022-11-23 00:34:21 +0100

    Created login stage layout

---
+++ JavaFX/src/javafx/Main.java
@@ -0,0 +1,29 @@
+package javafx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application{  
+    @Override  
+    public void start(Stage primaryStage) throws Exception {  
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }  
+    public static void main(String[] args) {  
+        launch(args);  
+          
+    }  
+    }  
