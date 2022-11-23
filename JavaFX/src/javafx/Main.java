02e18fc7522c82aeae4f6b0c1fcaa55c2d70a5c6
empty-codes <92234322+empty-codes@users.noreply.github.com>
2022-11-23 10:55:54 +0100

    added comments explaining code

--- JavaFX/src/javafx/Main.java
+++ JavaFX/src/javafx/Main.java
@@ -11,19 +11,21 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+
+//allows us get core functionality of javafx from the class application
 public class Main extends Application{  
-    @Override  
+    @Override  //application calls a method called start that's overriden cause its inherited from application class
     public void start(Stage primaryStage) throws Exception {  
 
 
-        Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
-        Scene scene = new Scene(root);
+        Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml")); //attachs fxml file
+        Scene scene = new Scene(root); //Scene class, first parameter is your layout (other 2 are size of your scene if included)
 
         primaryStage.setScene(scene);
-        primaryStage.show();
+        primaryStage.show(); // used to actually display the scene (i guess it's similar to System.out.println?)
     }  
     public static void main(String[] args) {  
-        launch(args);  
+        launch(args);  //sets up program as a JavaFX app
           
     }  
     }  
