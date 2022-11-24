26ff4bead88fabdaca9c0ffdd4df569e7a29ee7a
empty-codes <92234322+empty-codes@users.noreply.github.com>
2022-11-24 02:25:35 +0100

    enabled switching between login and create account scenes
    -Renamed LoginSceneController to MainSceneController
    
    -added methods to allow switching between scenes when login and create account links clinked

---
+++ JavaFX/src/javafx/MainSceneController.java
@@ -0,0 +1,40 @@
+package javafx;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainSceneController {
+
+ private Stage stage;
+ private Scene scene;
+ private Parent root;
+ 
+ /* The reason that you need to do something about the IOException is that it is a checked exception. 
+If you call a method that can throw an exception,you're required to either catch it (using try catch), or
+declare that you're going to rethrow it (i.e tell the compiler that you know about the exception, 
+but you don't plan to do anything about it) 
+so that if your file is not found then the program doesnt blow up and gives you an error message. */
+
+
+ public void switchToLoginScene(ActionEvent event) throws IOException {
+  root = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
+  stage = (Stage)((Node)event.getSource()).getScene().getWindow(); //get source of event and cast it to a node
+  scene = new Scene(root);
+  stage.setScene(scene);
+  stage.show();
+ }
+ 
+ public void switchToCreateAccountScene(ActionEvent event) throws IOException {
+  Parent root = FXMLLoader.load(getClass().getResource("CreateAccountScene.fxml"));
+  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+  scene = new Scene(root);
+  stage.setScene(scene);
+  stage.show();
+ }
+}
