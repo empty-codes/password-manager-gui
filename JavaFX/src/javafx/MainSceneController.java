61cc30ce81d00d95ee59b60290c21e77dfb914cd
empty-codes <92234322+empty-codes@users.noreply.github.com>
2022-12-27 12:58:09 +0100

    added methods that specify what each icon does when clicked

--- JavaFX/src/javafx/MainSceneController.java
+++ JavaFX/src/javafx/MainSceneController.java
@@ -3,10 +3,12 @@ package javafx;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class MainSceneController {
@@ -14,6 +16,7 @@ public class MainSceneController {
  private Stage stage;
  private Scene scene;
  private Parent root;
+
  
  /* The reason that you need to do something about the IOException is that it is a checked exception. 
 If you call a method that can throw an exception,you're required to either catch it (using try catch), or
@@ -53,5 +56,33 @@ public void switchToSecQuestionsScene(ActionEvent event) throws IOException {
    stage.setScene(scene);
    stage.show();
   }
+
+@FXML
+//function for mimimize icon
+private void minimizeWindow(MouseEvent event) {
+   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+   stage.setIconified(true); //iconifies it to the taskbar; turns it into an icon from a window
+}
+
+@FXML
+//function for maximize icon
+private void maximizeWindow(MouseEvent event) {
+   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+   //checks if stage is maximized, maximizes it if it isn't and sets it to the default stage size if it is (restore down)
+   if(stage.isMaximized()) {
+      stage.setMaximized(false);
+   }else {
+      stage.setMaximized(true);
+   }
+}
+
+@FXML
+//function for cancel icon
+private void closeWindow(MouseEvent event) {
+   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+   javafx.application.Platform.exit(); /*JavaFX launcher will call the Application stop method and terminate the JavaFX application thread.
+    The launcher thread will then shutdown. */
+}
+     
 
 }
