8c419ef605493a09b3d2c2979c727cde81050dbb
empty-codes <92234322+empty-codes@users.noreply.github.com>
2022-12-27 12:58:25 +0100

    removed unused imports

--- JavaFX/src/javafx/Main.java
+++ JavaFX/src/javafx/Main.java
@@ -1,15 +1,11 @@
 package javafx;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 //allows us get core functionality of javafx from the class application
@@ -21,9 +17,11 @@ public class Main extends Application{
         Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml")); //attachs fxml file of scene you initially want to display
         Scene scene = new Scene(root); //Scene class, first parameter is your layout (other 2 are size of your scene if included)
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED); // removes the default minimize, maximize and close buttons
         stage.show(); // used to actually display the scene (i guess it's similar to System.out.println?)
     }  
     public static void main(String[] args) {  
+
         launch(args);  //sets up program as a JavaFX app
           
     }  
